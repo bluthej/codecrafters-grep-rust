fe6837e231fb61988d12794aff72434ac7faf601
bluthej <joffrey.bluthe@e.email>
2023-10-22 23:20:17 +0200

    Match positive character groups

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
         pat if pat.chars().count() == 1 => input_line.contains(pattern),
         r"\d" => input_line.chars().any(|c| c.is_ascii_digit()),
         r"\w" => input_line.chars().any(|c| c.is_ascii_alphanumeric()),
+        pat if pat.starts_with('[') && pat.ends_with(']') => {
+            let chars = pat
+                .strip_prefix('[')
+                .expect("`pat` starts with [")
+                .strip_suffix(']')
+                .expect("`pat` ends with ]");
+            input_line.chars().any(|c| chars.contains(c))
+        }
         _ => panic!("Unhandled pattern: {}", pattern),
     }
 }
