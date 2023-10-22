f11508bde6c8280866b7c7ab686882d4e408669e
bluthej <joffrey.bluthe@e.email>
2023-10-22 21:57:31 +0200

    Match alphanumeric characters

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,11 @@ use std::io;
 use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        input_line.contains(pattern)
-    } else if pattern == r"\d" {
-        input_line.chars().any(|c| c.is_ascii_digit())
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
+    match pattern {
+        pat if pat.chars().count() == 1 => input_line.contains(pattern),
+        r"\d" => input_line.chars().any(|c| c.is_ascii_digit()),
+        r"\w" => input_line.chars().any(|c| c.is_ascii_alphanumeric()),
+        _ => panic!("Unhandled pattern: {}", pattern),
     }
 }
 
@@ -24,9 +23,5 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    if match_pattern(&input_line, &pattern) {
-        process::exit(0)
-    } else {
-        process::exit(1)
-    }
+    process::exit((!match_pattern(&input_line, &pattern)).into())
 }
