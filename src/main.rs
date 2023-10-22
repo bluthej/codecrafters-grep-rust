cd746363597ab471e77a98b291771f031841ce83
bluthej <joffrey.bluthe@e.email>
2023-10-22 15:53:51 +0200

    Match digits

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::process;
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         input_line.contains(pattern)
+    } else if pattern == r"\d" {
+        input_line.chars().any(|c| c.is_ascii_digit())
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
